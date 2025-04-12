0f6d023b8737f412c2571f5af007381c7d1828dd
jiang-h-y <hdyjiang@gmail.com>
2025-04-12 12:02:57 -0400

    editing ui for admin

--- app/src/pages/15_edit_student_data.py
+++ app/src/pages/15_edit_student_data.py
@@ -3,22 +3,30 @@ logger = logging.getLogger(__name__)
 import streamlit as st
 from modules.nav import SideBarLinks
 import requests
+import pandas as pd
 
-# Call the SideBarLinks from the nav module in the modules directory
+API_LINK = "http://api:4000/admin/studentdata"
+
+# add side bar links
 SideBarLinks()
 
 # set the header of the page
 st.header("Student Data")
 
-student_data = requests.get("http://api:4000/admin/studentdata").json()
+# add a dataframe of student data
+student_data = pd.DataFrame(requests.get(API_LINK).json())
+ordered_cols = ["user_id", "nu_id", "first_name", "MI", "last_name", "status"]
+student_data = [student_data[col] for col in ordered_cols]
 with st.expander("View All Student Data"):
     st.dataframe(student_data)
 
 # add spacing for visual clarity
 st.text("")
 
+# add columns for adding/removing student data
 col1, col2 = st.columns(2)
 
+# create a form to submit additional student data
 with col1:
     st.write("Add New Student to System")
     with st.form("add_student_form"):
@@ -37,12 +45,14 @@ with col1:
                 "ln": last_name
             }
 
-            response = requests.post("http://api:4000/admin/studentdata", json=data)
+            response = requests.post(API_LINK, 
+                                     json=data)
             if response.status_code == 200:
                 st.success("Student successfully added.")
             else:
                 st.error(f"Failed to add student.")
 
+# create a form to remove student data
 with col2:
     st.write("Remove Student from System")
     with st.form("remove_student_form"):
@@ -56,8 +66,7 @@ with col2:
             else:
                 data = {"user_id": user_id}
 
-                response = requests.delete("http://api:4000/admin/studentdata", 
-                                            json=data)
+                response = requests.delete(API_LINK, json=data)
                 if response.status_code == 200:
                     st.success("Student data successfully deleted.")
                 else:
