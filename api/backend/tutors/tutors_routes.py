4d1ffb36fa4725618ef1cf2e1a7a4b2177293e8f
chinychin99 <biaktchin8@gmail.com>
2025-04-12 15:41:25 -0400

    updated add_bio page, add post to tutor route

--- api/backend/tutors/tutors_routes.py
+++ api/backend/tutors/tutors_routes.py
@@ -3,10 +3,15 @@ import json
 from backend.db_connection import db
 from backend.simple.playlist import sample_playlist_data
 
+
 # This blueprint handles some basic routes that you can use for testing
 tutors_routes = Blueprint('tutors_routes', __name__)
 
+
 # ------------------------------------------------------------
+# GET /transactions/{tutor_id} -------------------------------
+
+
 @tutors_routes.route('/transactions', methods=["GET"])
 def get_transactions():
     current_app.logger.info('GET / transactions route')
@@ -15,35 +20,49 @@ def get_transactions():
     cursor.execute(query)
     data = cursor.fetchall()
 
+
     response = make_response(jsonify(data))
     response.status_code = 200
     return response
 
-@tutors_routes.route("/tutors", methods=["POST"])
+
+# POST /tutors/{tutor_id} --------------------------------
+@tutors_routes.route("add_bio", methods=["POST"])
 def post_transactions():
     data = request.json
     current_app.logger.info(data)
 
-    user_id = data["user_id"]
-    nu_id = data["nu_id"]
-    first_name = data["fn"]
-    last_name = data["ln"]
+
+    # change
+    first_name = data["first_name"]
+    last_name = data["last_name"]
     bio = data["bio"]
 
+
     cursor = db.get_db().cursor()
-    query = """INSERT INTO Students (user_id, nu_id, first_name, last_name) 
-    VALUES (%s, %s, %s, %s)"""
+    query = """INSERT INTO Students (first_name, last_name, bio)
+    VALUES (%s, %s, %s)"""
     current_app.logger.info(query)
+
 
     try:
         cursor = db.get_db().cursor()
-        cursor.execute(query, (user_id, nu_id, first_name, last_name))
+        cursor.execute(query, (first_name, last_name, bio))
         db.get_db().commit()
 
-        response = make_response("Successfully added student")
+
+        response = make_response("Successfully added bio")
         response.status_code = 200
         return response
     except:
-        response = make_response("Failed to add student")
+        response = make_response("Failed to add bio")
         response.status_code = 400
         return response
+   
+# PUT /tutors/{tutor_id} ---------------------------------------------
+
+
+# DELETE /bookings/{user_id}/{booking_id} ----------------------------
+
+
+# GET /bookings/{user_id} ---------------------------------------------
--- app/src/pages/13_add_bio.py
+++ app/src/pages/13_add_bio.py
@@ -3,12 +3,53 @@ logger = logging.getLogger(__name__)
 import streamlit as st
 from modules.nav import SideBarLinks
 import requests
+import pandas as pd
+
+
+# for post/tutors add bio
+
 
 # Call the SideBarLinks from the nav module in the modules directory
 SideBarLinks()
 
+
 # set the header of the page
 st.header("Add Bio")
 
-backup_data = requests.get("http://api:4000/t/backupschedule").json()
-st.dataframe(backup_data)
+
+
+
+# add spacing for visual clarity
+st.text("")
+
+
+# format columns
+# col1 = st.columns(1)
+
+
+# adding new info to bio
+# with col1:
+st.write("Add information for bio")
+with st.form("add_tutor_bio"):
+    first_name = st.text_input('First Name:')
+    last_name = st.text_input('Last Name:')
+    bio = st.text_input['Bio:']
+       
+    submitted = st.form_submit_button("Submit")
+
+
+    if submitted:
+        data = {
+            "first_name": first_name,
+            "last_name": last_name,
+            "bio" : bio
+        }
+
+
+        response = requests.post("http://api:4000/t/add_bio", json=data)
+        if response.status_code == 200:
+            st.success("Bio successfully added.")
+        else:
+            st.error(f"Failed to add bio.")
+
+
