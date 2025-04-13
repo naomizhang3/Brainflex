e70a6f409ab993eed2f1929afa2f751d27cceb8b
Tiller2 <tillerwang@gmail.com>
2025-04-13 19:22:56 -0400

    Made user interface for GET /bookings route for advisor

--- app/src/pages/35_booking_ratings.py
+++ app/src/pages/35_booking_ratings.py
@@ -19,3 +19,8 @@ st.header("View Booking Ratings")
 # add spacing for visual clarity
 st.text("")
 
+# displays average rating for each tutor in a table
+ratings_df = requests.get("http://api:4000/a/bookings").json()
+ratings_df = pd.DataFrame(ratings_df)
+ratings_df.columns = ["Average Rating", "Tutor First Name", "Tutor Last Name", "Tutor ID"]
+st.dataframe(ratings_df)
