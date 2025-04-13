b2741c273b302d922772365d2e820bf9e608d299
Tiller2 <tillerwang@gmail.com>
2025-04-13 19:28:34 -0400

    Made user interface for POST /requests route for advisor

--- app/src/pages/32_send_requests.py
+++ app/src/pages/32_send_requests.py
@@ -18,3 +18,31 @@ st.header("Send Requests to Admin")
 
 st.text("")
 
+col1, = st.columns(1)
+
+# sending requests to admin
+with col1:
+    st.write("Send a New Request to Admin")
+    with st.form("send_request_form"):
+        request_id = st.text_input("Request ID: ")
+        description = st.text_input("Description: ")
+        sent_by = st.text_input("Sent By: ")
+        type_id = st.text_input("Type ID: ")
+
+        submitted = st.form_submit_button("Submit")
+
+        if submitted:
+            data = {
+                "request_id": request_id,
+                "description": description,
+                "sent_by": sent_by,
+                "type_id": type_id
+            }
+
+            response = requests.post("http://api:4000/a/requests", json = data)
+            #st.write("Status code:", response.status_code)
+            #st.write("Response body:", response.text)
+            if response.status_code == 200:
+                st.success("Request successfully sent")
+            else:
+                st.error("Failed to send request")
